b796d5932261cea5e2bbbeda7e1353fec7835879
WandyPrograming <163542812+Papitula@users.noreply.github.com>
2024-12-27 21:23:25 -0400

    Iconos, Menu

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 let subMenu = document.getElementById('nav_links');
 let body = document.body;
 
+// Alternar el menú con la función menu_toggle
 function menu_toggle() {
-
-        if (subMenu.classList.contains('show')) {
-                subMenu.classList.remove('show');
-                document.body.style.overflowY = 'auto';
-        }
-
-        else {
-                subMenu.classList.add('show');
-                document.body.style.overflowY = 'hidden';
-        }
-
+    if (subMenu.classList.contains('show')) {
+        subMenu.classList.remove('show');
+        document.body.style.overflowY = 'auto';
+    } else {
+        subMenu.classList.add('show');
+        document.body.style.overflowY = 'hidden';
+    }
 }
+
+
+// Ocultar el menú al hacer clic en un enlace
+let enlaces = subMenu.querySelectorAll('a'); // Selecciona todos los enlaces dentro del subMenu
+
+enlaces.forEach(enlace => {
+    enlace.addEventListener('click', () => {
+        subMenu.classList.remove('show');
+        document.body.style.overflowY = 'auto';
+    });
+});
